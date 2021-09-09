e4ef302d4b867eef1b7c20f96b1986148fdf12ad
heymonicakay <hey.monicakay@gmail.com>
2021-09-08 20:15:10 -0500

    modified week two conditionals exercise instructions and updated workspace settings.json file

--- codeSandbox/exercises2/conditionals/src/index.js
+++ codeSandbox/exercises2/conditionals/src/index.js
@@ -1,24 +1,49 @@
 /*
-    👉 Hit RUN to view the instructions! 👈
-  (ignore any errors that may appear until you start coding)
+    💡 Codesandbox Tips 💡
+    ‣ Hide/Show left panel:
+      Click the cube icon in the top left of this window.
+    ‣ Run your code:
+      Your code will run automatically after you make changes.
+    ‣ View results of running your code:
+      Output will be printed to the console in the bottom right of this window.
+    ‣ Console Errors/Problems:
+      You can ignore any errors that appear while you are still typing.
 */
 
-const pastaIsMyFavorite = true;
+
+/*
+  PART 1: Pasta or spaghetti? 🤔
+  In the space below this commented section, write an if statement with multiple conditions.
+
+  If pasta IS your favorite food, and the restaurant HAS lasagna
+    Use console.log to display the following message:
+      "I will have the lasagna"
+
+  If only ONE of those conditions is true,
+    Use console.log to display the following statement:
+      "I will have the spaghetti"
+
+  If NEITHER condition is true,
+    Use console.log to display the following statement:
+      "I will have cake"
+*/
+
+const pastaIsMyFavorite = false;
 const restaurantHasLasagna = false;
 
 /*
-  Write an if statement, with multiple conditions that
-  uses console.log to display the statement, "I will have the lasagna"
-  if pasta is your favorite food, and the restaurant
-  has lasagna on the menu. If either of those conditions are false,
-  use console.log to display the statement, "I will have the spaghetti".
-*/
-
-/*
   PART 2: Is a hotdog a sandwich? 🤔
+  In the space below this commented section, write an if statement with multiple conditions.
+  💡 TIP: Comment out your code from part 1 to prevent your console from getting cluttered.
 
-  Write an if statement with multiple conditions. IF 'isHotdog' is true, AND 'isSandwich' is true, use console.log() to display the message: "A hotdog IS a sandwich!"
-  ELSE, use console.log() to display the message: "This is definitely not a sandwich."
+  IF 'isHotdog' is true, AND 'isSandwich' is true,
+    Use console.log() to display the following message:
+      "A hotdog IS a sandwich!"
+
+  ELSE,
+    Use console.log() to display the following message:
+      "This is definitely not a sandwich."
 */
+
 const isHotdog = true;
 const isSandwich = true;
